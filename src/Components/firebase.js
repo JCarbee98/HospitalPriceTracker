3387b8e9864b6000daaa22b1ed74dcdbad44a417
JCarbee98 <40305948+JCarbee98@users.noreply.github.com>
2019-04-01 14:11:18 -0400

    Firebase Partial Implementation
    -Installed firebase package
    -Created firebase.js with credentials
    NEED TO BE DONE: Actually creating the firebase object and implenting a database

---
+++ src/Components/firebase.js
@@ -0,0 +1,18 @@
+import app from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyDSvhYb9y4GGk0Al96eUUjtiQ5bxZ4FwJw",
+  authDomain: "chiron-f174a.firebaseapp.com",
+  databaseURL: "https://chiron-f174a.firebaseio.com",
+  projectId: "chiron-f174a",
+  storageBucket: "chiron-f174a.appspot.com",
+  messagingSenderId: "1074354402526",
+};
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import'./index.css';
-
-
+import Firebase, { FirebaseContext } from './Components/firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
