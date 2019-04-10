05f445c0bf7d573c7ebd36a99ec1e7ead7e1de27
srl16 <srl16@my.fsu.edu>
2019-04-10 00:59:19 -0400

    About Page information completed
    Finished adding the rest of the information and images for AboutPage.js

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -25,19 +25,35 @@ class AboutPage extends Component {
 						the best price!</p>
 				</div>
 				<div>
-					<p>Our idea was birthed from a new law that was passed on January 1, 2019.</p>
+					<p>Our idea was birthed from a new law that was passed on January 1, 2019. Emilio Murillo informed the rest
+					of us about it, and how it now requires all hospitals that take medicare/medicaid to publish complete price lists online.
+					This law, however, does not specify a format for the data; and prices for some hospitals can also be difficult to
+					find. So, the goal of Chiron Price Tracker is to provide an easy to use comparison search tool for multiple
+						different hospitals.</p>
+				</div>
+				<div>
+					<p>We chose the name 'Chiron' for our Hospital Operation Price Tracker because in Greek mythology, Chiron was famous
+					for his wisdom and knowledge of medicine. The initials CPT can also be used to represent 'Current Procedural Terminology',
+					which is used across the country as a uniform process for coding medical services.</p>
 				</div>
 
 
 
 				<h2>About Us</h2>
 				<div>
+					<p>All five members of this project are currently third year Computer Science students at Florida State
+						University. We are working together together on this project for our CEN4020 Software Engineering course.
+						</p>
+				</div>
+				<div>
 					<img src={image} alt="Image" />
 				</div>
 					From left to right: Emilio Murillo, Brandon Hahn, Spencer Lewis, and Tyler St Preux
 				<p></p>
-				<p>This is a class project for CEN4020 where our main goal is
-				to implement a webpage that allows the user to search average prices for operations! </p>
+				<div>
+					<img src={image2} alt="Image2" />
+				</div>
+					And who could forget the amazing Joshua Carbee!?
 			</div>
 
         );
