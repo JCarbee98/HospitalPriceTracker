05db74734d876baa232d03c6ed72a586f635fc7c
St-Preux <ty@wc-dhcp159d095.employee-secure.wireless.fsu.edu>
2019-04-23 12:47:33 -0400

    Updated Background
    Reverted background back to original color

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,11 +8,55 @@ var divStyle = {
 };
 
 // Basic information about the project in it's current iteration is listed on the homepage
+class HomePage extends Component {
+    render() {
+        return (
+			<div>
+                <div style={divStyle}>
+                    <h2>Welcome to Chiron Price Tracker</h2>
+                </div>
+                <div>
+                    <h4>This application is currently in development.</h4>
+                </div>
+                <div>
+                    <h4>To test our application in its current iteration, please select the Hospital Lookup tab above. Then enter
+                    the name of a hospital in Florida to view the average costs of each one of that Hospital's procedures/operations.</h4>
+                </div>
+                <div>
+                    <h4>To learn more about the project and the team behind it, please select the About tab above.</h4>
+                </div>
+			</div>
+        );
+    }
+}
+export default HomePage;
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// Style setting to center header
+var divStyle = {
+    width: "500px",
+    margin: "auto"
+};
+
+// Basic information about the project in it's current iteration is listed on the homepage
 
 class HomePage extends Component {
     render() {
         return (0
-		/*	<div>
+			<div>
 <<<<<<< HEAD
 					  <h2></h2>
 =======
@@ -30,9 +74,9 @@ class HomePage extends Component {
                     <h4>To learn more about the project and the team behind it, please select the About tab above.</h4>
                 </div>
 >>>>>>> 738d785daa5761301516b1a79460269309a4c99e
-			</div>*/
+			</div>
         );
     }
 }
 
-export default HomePage;
+export default HomePage;*/
